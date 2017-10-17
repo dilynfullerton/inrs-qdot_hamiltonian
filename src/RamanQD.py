5aa3f343b21310736f25de01814a446469c0cd5b
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-10-17 15:19:59 -0400

    Work on implementation of analytic formulas for the Raman cross section

--- src/RamanQD.py
+++ src/RamanQD.py
@@ -5,6 +5,7 @@ the definitions in Riera
 import numpy as np
 import itertools as it
 from scipy import integrate as integ
+from scipy import optimize as opt
 from HamiltonianQD import J, K
 
 
@@ -171,72 +172,211 @@ class Raman:
         raise NotImplementedError  # TODO
 
 
-class RamanQD(Raman):
-    def __init__(self, r_0, constants, V, energies, states, istate, fstates,
-                 lifetimes_elec, lifetimes_hole, refidx_func=None):
-        super(RamanQD, self).__init__(
-            V=V, energies=energies, states=states, istate=istate,
-            fstates=fstates, lifetimes_elec=lifetimes_elec,
-            lifetimes_hole=lifetimes_hole, refidx_func=refidx_func,
-        )
-        self.r_0 = r_0
-        self._const = constants
+class RamanQD:
+    def __init__(self):
+        self.sigma_0 = 0  # TODO
+        self.E_0 = 0  # TODO
+        self.delta_f = 0  # TODO
+        self.S_0 = 0  # TODO
+        self.g = 0  # TODO
+        self.beta_1 = 0  # TODO
+        self.r_0 = 0  # TODO
 
-    def matelt_Hjl(self, bra, ket, j, omega_l, e_l):
-        n1i, l1i, m1i = self.get_state(state=ket, j=1)
-        n2i, l2i, m2i = self.get_state(state=ket, j=2)
-        n1f, l1f, m1f = self.get_state(state=bra, j=1)
-        n2f, l2f, m2f = self.get_state(state=bra, j=2)
-        if j == 1 and l1f == l2i and m1f == m2i:
-            t0 = self.T(l=l1f, na=n1f, nb=n2i, nu=1/2)
-        elif j == 2 and l1i == l2f and m1i == m2f:
-            t0 = self.T(l=l2f, na=n2f, nb=n1i, nu=1/2)
-        else:
-            return 0
-        t1 = (
-            abs(self._const.e) / self._mu(0) *
-            np.sqrt(2*np.pi / self.V / omega_l) *
-            np.dot(e_l, self._p_cv(0))
-        )
-        return t0 * t1
-
-    def matelt_Hjs(self, bra, ket, j, omega_s, e_s):
+    def cross_section(self, omega_s):
         return sum(
-            (self._matelt_Hjs_p(bra, ket, j=j, omega_s=omega_s, e_s=e_s, p=i)
-             for i in range(1, 4)),
-            0
-        )
+            (self._cross_section(omega_s=omega_s, p=p) for p in range(4)), 0)
 
-    def _matelt_Hjs_p(self, bra, ket, j, omega_s, e_s, p):
-        ni, li, mi = self.get_state(ket, j=j)
-        nf, lf, mf = self.get_state(bra, j=j)
-        if p == 1 and mf == mi + 1:
-            t0 = np.dot(e_s, BASIS_e_p)
-        elif p == 2 and mf == mi - 1:
-            t0 = np.dot(e_s, BASIS_e_m)
-        elif p == 3 and mf == mi:
-            t0 = np.dot(e_s, BASIS_e_z)
+    def _cross_section(self, omega_s, p):
+        t0 = 27/4 * self.sigma_0 * (omega_s/self.E_0)**2 * self.delta_f
+        if p == 0:
+            t1 = 0
+            for se, sh in it.product(self.estates(), self.hstates()):
+                M11 = self.Mj(1, 1, estate=se, hstate=sh, omega_s=omega_s)
+                M12 = self.Mj(1, 2, estate=se, hstate=sh, omega_s=omega_s)
+                M21 = self.Mj(2, 1, estate=se, hstate=sh, omega_s=omega_s)
+                M22 = self.Mj(2, 2, estate=se, hstate=sh, omega_s=omega_s)
+                t2 = self.S_0 / (self.g**4 + self.delta_f**2)
+                t1 += t2 * (
+                    M11.real * M22.real + M12.real * M21.real +
+                    M11.imag * M22.imag + M12.imag * M21.imag
+                )
+            return t0 * t1
         else:
-            return 0
-        if lf == li + 1:
-            t1 = Pi(1, p, l=li, m=mi)
-        elif lf == li - 1:
-            t1 = Pi(2, p, l=li, m=mi)
-        else:
-            return 0
-        t2 = (
-            1j*(-1)**j * abs(self._const.e) / self.r_0 / self._mu(j=j) *
-            np.sqrt(2*np.pi / self.V / omega_s)
-        )
-        t3 = self.I(lf=lf, nf=nf, li=li, ni=ni)
-        return t0 * t1 * t2 * t3
+            t1 = 0
+            for se, sh in it.product(self.estates(), self.hstates()):
+                M1p = self.Mj(1, p, estate=se, hstate=sh, omega_s=omega_s)
+                M2p = self.Mj(2, p, estate=se, hstate=sh, omega_s=omega_s)
+                t2 = self.S(p) / (self.g**4 + self.delta_f**2)
+                t1 += abs(M1p + M2p)**2 * t2
+            return t0 * t1
 
-    def I(self, lf, nf, li, ni, nu=1/2):
+    def S(self, p):
+        return 0  # TODO
+
+    def states(self, j):
+        return ()  # TODO
+
+    def estates(self):
+        return self.states(j=1)
+
+    def hstates(self):
+        return self.states(j=2)
+
+    def get_numbers(self, state):
+        return 0, 0, 0  # TODO
+
+    def Mj(self, j, p, estate, hstate, omega_s):
+        l1i, m1i, n1i = self.get_numbers(state=estate)
+        l2i, m2i, n2i = self.get_numbers(state=hstate)
+        if j == 2:
+            l1i, m1i, n1i, l2i, m2i, n2i = l2i, m2i, n2i, l1i, m1i, n1i
+        t1 = 0
+        for festate in self.estates():
+            l1f, m1f, n1f = self.get_numbers(state=festate)
+            # Continue if zero (check delta functions)
+            if m1f != m2i or l1f != l2i:
+                continue
+            elif p == 1 and m1f != m1i + 1:
+                continue
+            elif p == 2 and m1f != m1i - 1:
+                continue
+            elif p == 3 and m1f != m1i:
+                continue
+            # Evaluate terms
+            if l1f == l1i + 1:
+                t11_num = Pi(1, p, l=l1i, m=m1i)  # TODO: verify correctness
+            elif l1f == l1i - 1:
+                t11_num = Pi(2, p, l=l1i, m=m1i)  # TODO: verify correctness
+            else:
+                continue
+            t11_denom = (
+                omega_s / self.E_0 + self.beta(j) *
+                (self.x(l=l1i, n=n1i, j=j)**2 - self.x(l=l1f, n=n1f, j=j)**2)
+            )
+            a = self.get_index(state=festate, j=j)  # TODO I have no idea if this is what 'a' means
+            if a == 1:
+                t11_denom += 1j
+            t12 = (self.I(l1f, n1f, l1i, n1i, j=j) *
+                   self.T(l1f, n1f, n2i, j=j, nu=1/2))
+            t1 += t11_num / t11_denom * t12
+        t0 = -self.beta(j)
+        return complex(t0 * t1)
+
+    def x(self, l, n, j):
+        return 0  # TODO
+
+    def y(self, l, n, j):
+        return 0  # TODO
+
+    def _solve_roots_xy(self, l, j):
+        fun, jac = self.get_rootf_rootdf_xy(l=l, j=j)
+        roots = []
+        for x0y0 in self.expected_roots_xy(l=l, j=j):
+            assert opt.check_grad(func=fun, grad=jac, x0=x0y0)
+            result = opt.root(fun=fun, jac=jac, x0=x0y0)
+            if not result.success:
+                print('FAILED')  # TODO
+            else:
+                roots.append(result.x)
+        return roots
+
+    def expected_roots_xy(self, l, j):
+        return []  # TODO
+
+    def get_rootf_rootdf_xy(self, l, j):
+        rf1 = self._get_rootf1(l=l, j=j)
+        rf2 = self._get_rootf2(j=j)
+
+        def rf(xy):
+            x, y = xy
+            f1 = rf1(x, y)
+            f2 = rf2(x, y)
+            return np.array([f1, f2])
+
+        def rdf(xy):
+            x, y = xy
+            f1x = rf1(x, y, partial_x=1)
+            f1y = rf1(x, y, partial_y=1)
+            f2x = rf2(x, y, partial_x=1)
+            f2y = rf2(x, y, partial_y=1)
+            return np.array([[f1x, f1y],
+                             [f2x, f2y]])
+
+        return rf, rdf
+
+    def _get_rootf1(self, l, j):
+        l = l+1/2
+        mu0 = self.mu_0(j)
+        mui = self.mu_i(j)
+
+        def rf1(x, y, partial_x=0, partial_y=0):
+            if partial_x == 0 and partial_y == 0:
+                return (
+                    mu0 * x * J(l, d=1)(x) * K(l)(y) -
+                    mui * y * J(l)(x) * K(l, d=1)(y)
+                )
+            elif (partial_x, partial_y) == (1, 0):
+                return (
+                    mu0 * (J(l, d=1)(x) + x * J(l, d=2)(x)) * K(l)(y) -
+                    mui * y * J(l, d=1)(x) + K(l, d=1)(y)
+                )
+            elif (partial_x, partial_y) == (0, 1):
+                return (
+                    mu0 * x * J(l, d=1)(x) * K(l, d=1)(y) -
+                    mui * (K(l, d=1)(y) + y * K(l, d=2)(y)) * J(l)(x)
+                )
+            else:
+                raise RuntimeError  # TODO
+        return rf1
+
+    def _get_rootf2(self, j):
+        mu0 = self.mu_0(j)
+        mui = self.mu_i(j)
+        vj = self.V_j(j)
+
+        def _fx2(x, deriv=0):
+            if deriv == 0:
+                return x**2
+            elif deriv == 1:
+                return 2*x
+            elif deriv == 2:
+                return 2
+            elif deriv > 2:
+                return 0
+
+        def rf2(x, y, partial_x=0, partial_y=0):
+            if partial_x == 0 and partial_y == 0:
+                tc = 2 * mu0 * self.r_0**2 * vj
+            else:
+                tc = 0
+            # TODO: verify formula
+            return (
+                _fx2(y, deriv=partial_y) +
+                mu0 / mui * _fx2(x, deriv=partial_x) - tc)
+
+        return rf2
+
+    def V_j(self, j):
+        return 0  # TODO
+
+    def mu_0(self, j):
+        return 0  # TODO
+
+    def mu_i(self, j):
+        return 0  # TODO
+
+    def beta(self, j):
+        return 0  # TODO
+
+    def get_index(self, state, j):
+        return 0  # TODO
+
+    def I(self, lf, nf, li, ni, j, nu=1/2):
         r0 = self.r_0
-        xi = self.x(l=li, n=ni)
-        xf = self.x(l=lf, n=nf)
-        yi = self.y(l=li, n=ni)
-        yf = self.y(l=lf, n=nf)
+        xi = self.x(l=li, n=ni, j=j)
+        xf = self.x(l=lf, n=nf, j=j)
+        yi = self.y(l=li, n=ni, j=j)
+        yf = self.y(l=lf, n=nf, j=j)
         Jli = J(l=li+nu)
         Jlf = J(l=lf+nu)
         Kli = K(l=li+nu)
@@ -251,40 +391,21 @@ class RamanQD(Raman):
         ans_k = integ.quad(ifn_k, a=self.r_0, b=np.inf)[0]
 
         return (
-            self.A(l=li, n=ni) * self.A(l=lf, n=nf) * ans_j +
-            self.B(l=li, n=ni) * self.B(l=lf, n=nf) * ans_k
+            self.A(l=li, n=ni, j=j) * self.A(l=lf, n=nf, j=j) * ans_j +
+            self.B(l=li, n=ni, j=j) * self.B(l=lf, n=nf, j=j) * ans_k
         )
 
-    def _mu(self, j):
-        """Effective mass
-            j=0 : Surrounding medium
-            j=1 : Electron
-            j=2 : Hole
-        :param j: Labeling described above
-        :return: The effective mass of the medium, electron, or hole
-        """
-        return 0  # TODO
+    def T(self, l, na, nb, j, nu=1/2):
+        return self.I(lf=l, nf=nb, li=l, ni=na, j=j, nu=nu)
 
-    def _p_cv(self, x):
-        return np.zeros(3)  # TODO
-
-    def T(self, l, na, nb, nu):
-        return self.I(lf=l, nf=nb, li=l, ni=na, nu=nu)
-
-    def x(self, l, n):
-        return 0  # TODO
-
-    def y(self, l, n):
-        return 0  # TODO
-
-    def _Jl_Kl(self, l, n):
+    def _Jl_Kl(self, l, n, j):
         l = l+1/2
-        return J(l=l)(self.x(l=l, n=n)), K(l=l)(self.y(l=l, n=n))
+        return J(l=l)(self.x(l=l, n=n, j=j)), K(l=l)(self.y(l=l, n=n, j=j))
 
-    def A(self, l, n):
-        Jl, Kl = self._Jl_Kl(l=l, n=n)
+    def A(self, l, n, j):
+        Jl, Kl = self._Jl_Kl(l=l, n=n, j=j)
         return 1/np.sqrt(abs(Jl)**2 + (Jl/Kl)**2 * abs(Kl)**2)
 
-    def B(self, l, n):
-        Jl, Kl = self._Jl_Kl(l=l, n=n)
-        return Jl/Kl * self.A(l=l, n=n)
+    def B(self, l, n, j):
+        Jl, Kl = self._Jl_Kl(l=l, n=n, j=j)
+        return Jl/Kl * self.A(l=l, n=n, j=j)
