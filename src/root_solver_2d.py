b6529baef01be6d0843a3f1e684127ab671dc484
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-10-26 17:22:37 -0400

    Added real root solver

--- src/root_solver_2d.py
+++ src/root_solver_2d.py
@@ -4,7 +4,7 @@ from scipy import optimize as opt
 
 class RootSolverComplex2d:
     def _solve_roots_xy(self, *args, **kwargs):
-        fun = self._get_root_function(*args, **kwargs)
+        fun = self._get_root_function_cplx(*args, **kwargs)
         for x0y0 in self._get_expected_roots_xxyy(*args, **kwargs):
             result = opt.root(fun=fun, x0=x0y0)
             if not result.success:
@@ -22,10 +22,7 @@ class RootSolverComplex2d:
     def _get_expected_roots_xy(self, *args, **kwargs):
         raise NotImplementedError
 
-    def _get_root_function(self, *args, **kwargs):
-        fun_f = self._get_root_func1(*args, **kwargs)
-        fun_g = self._get_root_func2(*args, **kwargs)
-
+    def _get_root_function_cplx(self, *args, **kwargs):
         def pi(zz):
             z1, z2 = zz
             return np.array([z1.real, z1.imag, z2.real, z2.imag])
@@ -34,16 +31,59 @@ class RootSolverComplex2d:
             xr, xi, yr, yi = xxyy
             return np.array([xr+1j*xi, yr+1j*yi])
 
-        def psi(zz):
-            z1, z2, = zz
-            f = complex(fun_f(z1, z2))
-            g = complex(fun_g(z1, z2))
-            return np.array([f, g])
+        psi = self._get_root_function(*args, **kwargs)
 
         def rf(xxyy):
             return pi(psi(ipi(xxyy)))
 
         return rf
+
+    def _get_root_function(self, *args, **kwargs):
+        fun_f = self._get_root_func1(*args, **kwargs)
+        fun_g = self._get_root_func2(*args, **kwargs)
+
+        def psi(zz):
+            z1, z2, = zz
+            f = complex(fun_f(z1, z2))
+            g = complex(fun_g(z1, z2))
+            return np.array([f, g])
+        return psi
+
+    def _get_root_func1(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _get_root_func2(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class RootSolverReal2d:
+    def _solve_roots_xy(self, *args, **kwargs):
+        fun = self._get_root_function(*args, **kwargs)
+        for x0y0 in self._get_expected_roots_xxyy(*args, **kwargs):
+            result = opt.root(fun=fun, x0=x0y0)
+            if not result.success:
+                print('FAILED')  # TODO
+            else:
+                x, y = result.x
+                yield np.array([x, y])
+
+    def _get_expected_roots_xxyy(self, *args, **kwargs):
+        for x0, y0 in self._get_expected_roots_xy(*args, **kwargs):
+            yield np.array([x0, y0])
+
+    def _get_expected_roots_xy(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _get_root_function(self, *args, **kwargs):
+        fun_f = self._get_root_func1(*args, **kwargs)
+        fun_g = self._get_root_func2(*args, **kwargs)
+
+        def psi(xy):
+            x, y, = xy
+            f = fun_f(x, y)
+            g = fun_g(x, y)
+            return np.array([f, g])
+        return psi
 
     def _get_root_func1(self, *args, **kwargs):
         raise NotImplementedError
