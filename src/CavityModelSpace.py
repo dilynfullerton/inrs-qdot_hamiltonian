d27dc51ba1212d088c049c9c3937965954103e82
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-11-16 15:27:20 -0500

    Scalar potential for cavity

--- src/CavityModelSpace.py
+++ src/CavityModelSpace.py
@@ -2,6 +2,7 @@
 Definition for a simple harmonic optical cavity, which provides
 functions for interactions with phonon modes and electron/hole modes
 """
+import numpy as np
 from ModelSpace import ModelSpace
 from collections import namedtuple
 
@@ -10,12 +11,13 @@ CavityMode = namedtuple('CavityMode', ['n'])
 
 
 class CavityModelSpace(ModelSpace):
-    def __init__(self, l, w, h, omega_c):
+    def __init__(self, l, w, h, omega_c, g_e):
         self.length = l  # space between antennae
         self.width = w
         self.height = h
         self.area = 2 * l * (w + h)  # exposed area
         self.omega_c = omega_c
+        self.g_e = g_e
 
     def nfock(self, mode):
         return 2
@@ -38,3 +40,17 @@ class CavityModelSpace(ModelSpace):
             return self.omega_c
         else:
             return 0
+
+    def potential(self, mode):
+        # TODO: Find a theoretically-justified potential
+        def phi(x):
+            omega = self.get_omega(mode)
+            if mode.n == 1:
+                return self.g_e * self.length / omega / 2 * (
+                    np.exp(self.get_omega(mode) * (-x/self.length - 1/2)) +
+                    np.exp(self.get_omega(mode) * (x/self.length - 1/2))
+                )
+            else:
+                return 0
+        return phi
+
